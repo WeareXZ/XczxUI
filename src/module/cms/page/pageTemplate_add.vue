453394b71da1c3e492a95028ebe4594cafa4cb4f
WeareXZ <heyouze112233@163.com>
2021-04-17 18:47:40 +0800

    模板管理页面+页面预览

---
+++ src/module/cms/page/pageTemplate_add.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <!--编写页面静态部分，即view部分-->
+    <el-form :model="pageForm" label-width="80px" :rules="pageFormRules" ref="pageForm">
+      <el-form-item prop="siteId" label="所属站点">
+        <el-select v-model="pageForm.siteId" placeholder="请选择站点">
+          <el-option
+            v-for="item in siteList"
+            :key="item.siteId"
+            :label="item.siteName"
+            :value="item.siteId">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item prop="templateId" label="选择模板">
+        <el-select v-model="pageForm.templateId" placeholder="请选择模板">
+          <el-option
+            v-for="item in templateList"
+            :key="item.templateId"
+            :label="item.templateName"
+            :value="item.templateId">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item prop="pageName" label="页面名称">
+        <el-input v-model="pageForm.pageName" auto-complete="false"></el-input>
+      </el-form-item>
+      <el-form-item prop="pageAliase" label="页面别名">
+        <el-input v-model="pageForm.pageAliase" auto-complete="false"></el-input>
+      </el-form-item>
+      <el-form-item label="访问路径" prop="pageWebPath">
+        <el-input v-model="pageForm.pageWebPath" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="物理路径" prop="pagePhysicalPath">
+        <el-input v-model="pageForm.pagePhysicalPath" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="数据路径" prop="dataUrl">
+        <el-input v-model="pageForm.dataUrl" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="类型">
+        <el-radio-group v-model="pageForm.pageType">
+          <el-radio class="radio" label="0">静态</el-radio>
+          <el-radio class="radio" label="1">动态</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="创建时间">
+        <el-date-picker type="datetime" placeholder="创建时间" v-model="pageForm.pageCreateTime"></el-date-picker>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog‐footer">
+      <el-button type="plain" @click="go_back" class="">返回</el-button>
+      <el-button type="primary" @click="addSubmit">提交</el-button>
+    </div>
+  </div>
+</template>
+<script>
+  /*编写页面静态部分，即model及vm部分。*/
+  import * as cmsApi from '../api/cms'
+
+  export default {
+    data() {
+      return {
+        siteList: [],//站点列表
+        templateList: [],//模板列表
+        pageForm: {
+          siteId: '',
+          templateId: '',
+          pageTemplate: '',
+          pageName: '',
+          pageAliase: '',
+          pageType: '',
+          pageWebPath: '',
+          pagePhysicalPath: '',
+          pageType: '',
+          dataUrl:'',
+          pageCreateTime: new Date()
+        },
+        pageFormRules: {
+          siteId: [
+            {
+              required: true,
+              message: '请选择站点',
+              trigger: 'blur'
+            }
+          ],
+          templateId: [
+            {
+              required: true,
+              message: '请选择模版',
+              trigger: 'blur'
+            }
+          ],
+          pageName: [
+            {
+              required: true,
+              message: '请输入页面名称',
+              trigger: 'blur'
+            }
+          ],
+          pageWebPath: [
+            {
+              required: true,
+              message: '请输入访问路径',
+              trigger: 'blur'
+            }
+          ],
+          pagePhysicalPath: [
+            {
+              required: true,
+              message: '请输入物理路径',
+              trigger: 'blur'
+            }
+          ]
+        }
+      }
+    },
+    methods: {
+      addSubmit: function () {
+        this.$refs['pageForm'].validate((valid) => {
+          if (valid) {
+            this.$confirm('确认提交吗？', '提示', {}).then(() => {
+              cmsApi.page_add(this.pageForm).then((res) => {
+                if (res.success) {
+                  this.$message({
+                      message: '提交成功',
+                      type: 'success'
+                    }
+                  )
+                  //清空表单
+                  this.$refs['pageForm'].resetFields();
+                } else if(res.message){
+                  this.$message.error(res.message);
+                }else {
+                  this.$message({
+                    message: '提交失败',
+                    type: 'false'
+                  })
+                }
+              })
+            })
+          } else {
+            this.$message({
+                message: '校验失败',
+                type: 'false'
+              }
+            )
+            return false;
+          }
+        })
+      },
+      go_back: function () {
+        this.$router.push({
+          path: '/cms/page/list',
+          query: {
+            page: this.$route.query.page,
+            siteId: this.$route.query.siteId,
+            pageAliase:this.$route.query.pageAliase,
+            pageName:this.$route.query.pageName
+          }
+        })
+      }
+    },
+    //mounted()方法是DOM元素渲染生成完成后调用
+    //created()方法是vue实例已创建但是DOM元素还没有渲染生成
+    mounted() {
+      //站点列表
+      this.siteList = [
+        {
+          siteId: '5a751fab6abb5044e0d19ea1',
+          siteName: '门户主站'
+        },
+        {
+          siteId: '102',
+          siteName: '测试站'
+        }
+      ]
+      //模板列表
+      this.templateList = [
+        {
+          templateId: '5a962b52b00ffc514038faf7',
+          templateName: '首页'
+        },
+        {
+          templateId: '5a962bf8b00ffc514038fafa',
+          templateName: '轮播图'
+        }
+      ]
+    }
+  }
+</script>
+<style>
+  /*编写页面样式，不是必须*/
+</style>
